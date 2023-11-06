8765bbadfb4194dc59fcad30eae031d4adbd9319
Alehub369 <lievanoalejandro@gmail.com>
2023-11-06 16:31:36 -0500

    Estructura del formulario de comentarios

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,8 +22,11 @@ class PageController extends AbstractController
     #[Route('/blog/{slug}', name: 'app_post')]
     public function post(Post $post): Response
     {
+        $form = $this->createForm(CommentType::class);
+
         return $this->render('page/post.html.twig', [
-            'post' => $post
+            'post' => $post,
+            'form' => $form->createView()
         ]);
     }
 }
